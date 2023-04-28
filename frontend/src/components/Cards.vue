5b291537f49ce7e2b6d14f293dbe277df3a02261
Fukurokudzu <sergei.fukurokudzu@gmail.com>
2023-04-28 20:43:26 +0500

    Lists in progress

---
+++ frontend/src/components/Cards.vue
@@ -0,0 +1,26 @@
+<template>
+  <li v-for="card in cards" :key="card.id">
+      <div class="card-body">
+        <div class="card-title">
+          <h5>{{ card.title }}</h5>
+        </div>
+        <div class="card-text">
+          {{ card.description }}
+        </div>
+      </div>
+  </li>
+</template>
+
+<script>
+export default {
+props: ['cards'],
+setup(props) {
+  const cards = props.cards
+  return { cards }
+}
+}
+</script>
+
+<style>
+
+</style>
